000925b5ac45c9ced5083b1826bb85b193e8b346
treasure-sky <ych0486666@gmail.com>
2025-08-30 02:49:00 +0900

    🎉 스프링 프로젝트 생성

---
+++ src/main/java/knu/team1/be/boost/BoostApplication.java
@@ -0,0 +1,12 @@
+package knu.team1.be.boost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoostApplication.class, args);
+    }
+}
---
+++ src/test/java/knu/team1/be/boost/BoostApplicationTests.java
@@ -0,0 +1,11 @@
+package knu.team1.be.boost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
