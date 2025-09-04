72491b757a6829c4e72effba4167f3e3633c7538
seoyoungjin23 <ascd050@gmail.com>
2025-09-04 21:53:14 +0900

    ✨(#5) 파일 다운로드 Presigned URL 발급 API 구현

--- src/main/java/knu/team1/be/boost/file/controller/FileController.java
+++ src/main/java/knu/team1/be/boost/file/controller/FileController.java
@@ -4,10 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.net.URI;
+import java.util.UUID;
 import knu.team1.be.boost.file.dto.FileRequest;
 import knu.team1.be.boost.file.dto.FileResponse;
 import knu.team1.be.boost.file.service.FileService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +42,18 @@ public class FileController {
         return ResponseEntity
             .created(location)
             .body(fileResponse);
+    }
+
+    @Operation(
+        summary = "다운로드 Presigned URL 발급",
+        description = "파일 ID를 기반으로 다운로드 가능한 S3 Presigned URL을 반환합니다."
+    )
+    @GetMapping("/{fileId}/download-url")
+    public ResponseEntity<FileResponse> downloadFile(
+        @PathVariable UUID fileId
+    ) {
+        FileResponse fileResponse = fileService.downloadFile(fileId);
+
+        return ResponseEntity.ok(fileResponse);
     }
 }
--- src/main/java/knu/team1/be/boost/file/service/FileServiceImpl.java
+++ src/main/java/knu/team1/be/boost/file/service/FileServiceImpl.java
@@ -2,6 +2,7 @@ package knu.team1.be.boost.file.service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -16,9 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.ServerSideEncryption;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 
 @Slf4j
@@ -79,6 +82,44 @@ public class FileServiceImpl implements FileService {
             key,
             presigned.url().toString(),
             "PUT",
+            headers,
+            expireSeconds
+        );
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public FileResponse downloadFile(UUID fileId) {
+        // TODO: 다운로드 요청자가 해당 프로젝트 팀원인지 검증
+
+        File file = fileRepository.findById(fileId)
+            .orElseThrow(() -> new IllegalArgumentException("파일을 찾을 수 없습니다: " + fileId));
+
+        if (file.getStatus() != FileStatus.COMPLETED) {
+            throw new IllegalArgumentException("아직 다운로드할 수 없는 상태의 파일입니다.");
+        }
+
+        String key = file.getStorageKey();
+
+        GetObjectRequest getReq = GetObjectRequest.builder()
+            .bucket(bucket)
+            .key(key)
+            .responseContentDisposition(
+                "attachment; filename=\"" + file.getOriginalFilename() + "\"")
+            .responseContentType(file.getContentType())
+            .build();
+
+        PresignedGetObjectRequest presigned = s3Presigner.presignGetObject(builder -> builder
+            .getObjectRequest(getReq)
+            .signatureDuration(Duration.ofSeconds(expireSeconds)));
+
+        Map<String, String> headers = Collections.emptyMap();
+
+        return new FileResponse(
+            file.getId(),
+            key,
+            presigned.url().toString(),
+            "GET",
             headers,
             expireSeconds
         );
