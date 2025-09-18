f00b77bdf5011d46e14e7ca69622e53ea7fd3af2
treasure-sky <ych0486666@gmail.com>
2025-09-18 21:02:16 +0900

    ♻️(#33) 참여코드 상태 ENUM 설명 추가

--- src/main/java/knu/team1/be/boost/projectMember/entity/CodeStatus.java
+++ src/main/java/knu/team1/be/boost/projectMember/entity/CodeStatus.java
@@ -1,7 +1,16 @@
 package knu.team1.be.boost.projectMember.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum CodeStatus {
-    ACTIVE, // 활성 상태
-    REVOKED, // 취소된 상태 (사용자에 의해 새로운 코드가 생성될 때 기존 코드 REVOKED)
-    EXPIRED // 만료된 상태 (코드 발급 후 일정시간이 지나면 만료)
+    ACTIVE("활성"),
+    REVOKED("철회"), // 사용자에 의해 새로운 코드가 생성될 때 기존 코드 REVOKED
+    EXPIRED("만료"); // 만료된 상태 코드 발급 후 일정시간이 지나면 만료
+
+    private final String description;
+
+    CodeStatus(String description) {
+        this.description = description;
+    }
 }
