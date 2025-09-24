4cb17bf9cf119b84c1cf906985a8f6866f7ed8db
kwh <kmwh030@gmail.com>
2025-09-24 17:41:52 +0900

    ♻️(#53) TokenDto builder -> 정적 팩토리 메소드

--- src/main/java/knu/team1/be/boost/auth/dto/TokenDto.java
+++ src/main/java/knu/team1/be/boost/auth/dto/TokenDto.java
@@ -1,11 +1,11 @@
 package knu.team1.be.boost.auth.dto;
 
-import lombok.Builder;
-
-@Builder
 public record TokenDto(
     String accessToken,
     String refreshToken
 ) {
 
+    public static TokenDto of(String accessToken, String refreshToken) {
+        return new TokenDto(accessToken, refreshToken);
+    }
 }
