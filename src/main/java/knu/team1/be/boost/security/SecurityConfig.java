d1b6533a56cabf4787f52c4730f43c029857e460
seoyoungjin23 <ascd050@gmail.com>
2025-10-15 16:08:59 +0900

    🚑️ CORS 설정에 로컬 호스트를 추가하여 개발 환경에서의 접근을 허용

--- src/main/java/knu/team1/be/boost/security/SecurityConfig.java
+++ src/main/java/knu/team1/be/boost/security/SecurityConfig.java
@@ -71,7 +71,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("https://boost.ai.kr"));
+        configuration.setAllowedOriginPatterns(
+            List.of("https://boost.ai.kr", "http://localhost:5173"));
         configuration.setAllowedMethods(
             List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
