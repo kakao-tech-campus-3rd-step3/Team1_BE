051a438199bad65608061ec2c5562774f432055e
KWH <kmwh030@gmail.com>
2025-10-16 16:19:46 +0900

    🩹 (#95) dto swagger 설명 추가

--- src/main/java/knu/team1/be/boost/memo/dto/MemoItemResponseDto.java
+++ src/main/java/knu/team1/be/boost/memo/dto/MemoItemResponseDto.java
@@ -1,13 +1,21 @@
 package knu.team1.be.boost.memo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import knu.team1.be.boost.memo.entity.Memo;
 
 public record MemoItemResponseDto(
+    @Schema(description = "메모 ID", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
     UUID id,
+
+    @Schema(description = "메모 제목", example = "메모 제목 1")
     String title,
+
+    @Schema(description = "메모 생성일", example = "2025-10-01T15:30:00")
     LocalDateTime createdAt,
+
+    @Schema(description = "메모 수정일", example = "2025-10-01T15:30:00")
     LocalDateTime updatedAt
 ) {
 
--- src/main/java/knu/team1/be/boost/memo/dto/MemoResponseDto.java
+++ src/main/java/knu/team1/be/boost/memo/dto/MemoResponseDto.java
@@ -1,14 +1,24 @@
 package knu.team1.be.boost.memo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import knu.team1.be.boost.memo.entity.Memo;
 
 public record MemoResponseDto(
+    @Schema(description = "메모 ID", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
     UUID id,
+
+    @Schema(description = "메모 제목", example = "메모 제목 1")
     String title,
+
+    @Schema(description = "메모 내용", example = "메모 내용입니다.")
     String content,
+
+    @Schema(description = "메모 생성일", example = "2025-10-01T15:30:00")
     LocalDateTime createdAt,
+
+    @Schema(description = "메모 수정일", example = "2025-10-01T15:30:00")
     LocalDateTime updatedAt
 ) {
 
