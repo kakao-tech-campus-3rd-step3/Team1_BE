d68e80788d314d90a885e23892b82badbc0e3cf7
KWH <kmwh030@gmail.com>
2025-10-16 16:07:38 +0900

    ✨ (#95) 에러 코드 추가

--- src/main/java/knu/team1/be/boost/common/exception/ErrorCode.java
+++ src/main/java/knu/team1/be/boost/common/exception/ErrorCode.java
@@ -45,6 +45,7 @@ public enum ErrorCode {
 
     // Memo 관련
     MEMO_NOT_FOUND(HttpStatus.NOT_FOUND, "Memo not found", "메모를 찾을 수 없습니다."),
+    PROJECT_MEMO_ONLY(HttpStatus.CONFLICT, "Memo not in project", "해당 메모가 프로젝트에 속하지 않습니다."),
 
     // Auth 관련
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid refresh token", "유효하지 않은 리프레시 토큰입니다."),
