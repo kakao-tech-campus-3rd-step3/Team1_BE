1a1142b6d26a9e4f9a0c24ef65eb03dc1933e8c7
KWH <kmwh030@gmail.com>
2025-10-16 16:46:02 +0900

    🩹 (#82) dto swagger 추가

--- src/main/java/knu/team1/be/boost/auth/dto/LoginResponseDto.java
+++ src/main/java/knu/team1/be/boost/auth/dto/LoginResponseDto.java
@@ -1,10 +1,16 @@
 package knu.team1.be.boost.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import knu.team1.be.boost.member.dto.MemberResponseDto;
 
 public record LoginResponseDto(
+    @Schema(description = "사용자 정보")
     MemberResponseDto memberResponseDto,
+
+    @Schema(description = "엑세스 토큰")
     String accessToken,
+
+    @Schema(description = "최초 로그인 여부")
     Boolean isNewUser
 ) {
 
