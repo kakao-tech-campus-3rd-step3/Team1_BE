f6ed244c7649705b11b980318174cefd5dbc5c2e
treasure-sky <ych0486666@gmail.com>
2025-10-20 19:46:49 +0900

    ♻️(#112) 회원 정보 수정 API를 이름과 아바타로 분리

--- src/main/java/knu/team1/be/boost/member/dto/MemberNameUpdateRequestDto.java
+++ src/main/java/knu/team1/be/boost/member/dto/MemberNameUpdateRequestDto.java
@@ -4,15 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record MemberUpdateRequestDto(
+public record MemberNameUpdateRequestDto(
     @Schema(description = "수정할 회원 이름", example = "김부스트")
     @Size(min = 1, max = 20, message = "이름은 1자 이상 20자 이하로 입력해주세요.")
     @NotBlank(message = "이름은 비워둘 수 없습니다.")
-    String name,
-
-    @Schema(description = "수정할 회원 아바타", example = "1112")
-    @NotBlank(message = "아바타는 비워둘 수 없습니다.")
-    String avatar
+    String name
 ) {
 
 }
