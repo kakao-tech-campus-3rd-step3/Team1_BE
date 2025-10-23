87cdeda629ef2024f87ec0985b0e629cf7b35203
seoyoungjin23 <ascd050@gmail.com>
2025-10-23 19:51:01 +0900

    ✨(#94) 알림 엔티티 구현

---
+++ src/main/java/knu/team1/be/boost/notification/entity/Notification.java
@@ -0,0 +1,41 @@
+package knu.team1.be.boost.notification.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import knu.team1.be.boost.common.entity.SoftDeletableEntity;
+import knu.team1.be.boost.member.entity.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Table(name = "notifications")
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE notifications SET deleted = true, deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted = false")
+public class Notification extends SoftDeletableEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private boolean read = false;
+
+}
