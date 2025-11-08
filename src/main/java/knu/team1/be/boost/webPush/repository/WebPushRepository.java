be239be14f2a0f540d05c77829f810b6373f8e17
seoyoungjin23 <ascd050@gmail.com>
2025-11-09 03:02:07 +0900

    🐛(#226) 회원 삭제 시 관련 알림 및 웹 푸시 구독 정보 소프트 삭제하도록 수정

--- src/main/java/knu/team1/be/boost/webPush/repository/WebPushRepository.java
+++ src/main/java/knu/team1/be/boost/webPush/repository/WebPushRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.UUID;
 import knu.team1.be.boost.webPush.entity.WebPushSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WebPushRepository extends JpaRepository<WebPushSubscription, UUID> {
 
@@ -12,5 +14,11 @@ public interface WebPushRepository extends JpaRepository<WebPushSubscription, UU
 
     List<WebPushSubscription> findByMemberId(UUID memberId);
 
-    Optional<WebPushSubscription> findByWebPushUrl(String webPushUrl);
+    @Modifying
+    @Query("""
+        UPDATE WebPushSubscription w
+        SET w.deleted = true, w.deletedAt = CURRENT_TIMESTAMP
+        WHERE w.member.id = :memberId AND w.deleted = false
+        """)
+    void softDeleteAllByMemberId(UUID memberId);
 }
