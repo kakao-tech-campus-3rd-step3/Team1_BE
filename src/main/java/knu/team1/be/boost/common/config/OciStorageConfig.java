670097e438a84901b7f8bdc1154e920aa2e08bd9
treasure-sky <ych0486666@gmail.com>
2025-12-02 23:36:40 +0900

    ✨ S3 설정에 경로 스타일 접근 활성화 추가

--- src/main/java/knu/team1/be/boost/common/config/OciStorageConfig.java
+++ src/main/java/knu/team1/be/boost/common/config/OciStorageConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -34,10 +35,15 @@ public class OciStorageConfig {
 
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
 
+        S3Configuration s3Config = S3Configuration.builder()
+            .pathStyleAccessEnabled(true)
+            .build();
+
         return S3Presigner.builder()
             .endpointOverride(URI.create(endpoint))
             .region(Region.of(region))
             .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .serviceConfiguration(s3Config)
             .build();
     }
 }
